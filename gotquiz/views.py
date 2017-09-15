a8309c607368b1324ab1d9b882191949958e6a6a
teresia <teresia.schullstrom@gmail.com>
2017-09-15 16:03:44 +0200

    lagt till data

--- gotquiz/views.py
+++ gotquiz/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
+quizzes = [
+	{
+		"quiz_number": 1,
+   		"name": "Klassiska böcker",
+	   	"description": "Hur bra kan du dina klassiker?"
+	},
+	{
+		"quiz_number": 2,
+   	   	"name": "Största fotbollslagen",
+	   	"description": "Kan du dina lag?"
+	},
+	{
+		"quiz_number": 3,
+   	    	"name": "Världens mest kända hackare",
+	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
+]
+
+
+
+def startpage(request):
+	context = {
+		"quizzes": quizzes,
+	}
+	return render(request, "quiz/index.html", context)
+
+def quiz(request, quiz_number):
+	context = {
+		"quiz": quizzes[int(quiz_number) - 1],
+		"quiz_number": quiz_number,
+	}
+
+	return render(request, "quiz/quiz.html", context)
+
+def question(request):
+	return render(request, "quiz/question.html")
+
+def completed(request):
+	return render(request, "quiz/results.html")
