94f7250093f2f60ec50652ba335e778bb8ec136e
teresia <teresia.schullstrom@gmail.com>
2017-09-17 12:22:23 +0200

    mytt ut tillfälliga dictionarie smot tillfällig databas

--- gotquiz/views.py
+++ gotquiz/views.py
@@ -1,46 +1,33 @@
 from django.shortcuts import render
-
-quizzes = [
-	{
-		"quiz_number": 1,
-   		"name": "Klassiska böcker",
-	   	"description": "Hur bra kan du dina klassiker?"
-	},
-	{
-		"quiz_number": 2,
-   	   	"name": "Största fotbollslagen",
-	   	"description": "Kan du dina lag?"
-	},
-	{
-		"quiz_number": 3,
-   	    	"name": "Världens mest kända hackare",
-	    	"description": "Hackerhistoria är viktigt, kan du den?"	},
-]
-
+from gotquiz.models import Quiz
 
 def startpage(request):
 	context = {
-		"quizzes": quizzes,
+		"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/index.html", context)
 
 def quiz(request, quiz_number):
 	context = {
-		"quiz": quizzes[int(quiz_number) - 1],
+	    "quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
+
 
 	return render(request, "quiz/quiz.html", context)
 
 def question(request, quiz_number, question_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	question = questions[int(question_number) - 1]
 	context = {
-		"question_number": question_number,
-	    "question": "Hur många bultar har ölandsbron?",
-		"answer1": "12",
-	   	"answer2": "66 400",
-	    "answer3": "7 428 954",
-	    "answer4": "9 428 954",
-	    "quiz_number": quiz_number,
+    	"question_number": question_number,
+    	"question": question.question,
+	    "answer1": question.answer1,
+    	"answer2": question.answer2,
+	    "answer3": question.answer3,
+	    "quiz": quiz,
+		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
 
